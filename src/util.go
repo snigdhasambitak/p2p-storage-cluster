f7e553fd670e9c7e1847f92a9e580d4b4e8ae213
Snigdha Aryakumar <saryakumar@travix.com>
2022-03-13 18:07:44 +0100

    change hash size

--- src/util.go
+++ src/util.go
@@ -10,7 +10,7 @@ import (
 	"math/rand"
 )
 
-const keySize = sha1.Size * 8
+const keySize = sha1.Size * 2
 
 var two = big.NewInt(2)
 var hashMod = new(big.Int).Exp(big.NewInt(2), big.NewInt(keySize), nil)
